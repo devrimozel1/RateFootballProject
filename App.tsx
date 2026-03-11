17c95425bcf0a4456ed8fa71a0ac2dd353ac6d5d
Cansu <cansuak39@gmail.com>
2026-03-11 16:44:30 +0300

    C4

--- App.tsx
+++ App.tsx
@@ -2,9 +2,8 @@ import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
-import Router from './src/Router';
+import Router from './src/navigation/Router';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
