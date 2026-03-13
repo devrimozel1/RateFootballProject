adc5dc648b2342eee28587de058f41be496dbdfe
Cansu <cansuak39@gmail.com>
2026-03-13 15:12:17 +0300

    C5

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,42 +1,145 @@
 import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import React, { useState } from 'react'
 import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import HeaderComp from './HeaderComp'
+import DropDownPicker from 'react-native-dropdown-picker'
 
 
 function Register() {
 
     const navigation = useNavigation()
     const ScreenName = "Register"
+
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+
+    const [items, setItems] = useState([
+        { label: 'Trendyol Süper Lig', value: 'TSL'},
+        { label: 'Trendyol 1.Lig', value: 'TL' },
+
+    ]);
+
+    const allTeams = [
+        { label: 'GALATASARAY A.Ş', lig: 'TSL', value: 'gs' },
+        { label: 'FENERBAHÇE A.Ş', lig: 'TSL', value: 'fb' },
+        { label: 'TRABZONSPOR A.Ş', lig: 'TSL', value: 'ts' },
+        { label: 'BEŞİKTAŞ', lig: 'TSL', value: 'bjk' },
+        { label: 'RAMS BAŞAKŞEHİR FUTBOL KULÜBÜ', lig: 'TSL', value: 'bşk' },
+        { label: 'GÖZTEPE A.Ş', lig: 'TSL', value: 'gzt' },
+        { label: 'KOCAELİSPOR', lig: 'TSL', value: 'koc' },
+        { label: 'SAMSUNSPOR A.Ş', lig: 'TSL', value: 'sam' },
+        { label: 'ÇAYKUR RİZESPOR A.Ş', lig: 'TSL', value: 'çrz' },
+        { label: 'GAZİANTEP FUTBOL KULÜBÜ A.Ş', lig: 'TSL', value: 'gaf' },
+        { label: 'CORENDON ALANYASPOR', lig: 'TSL', value: 'cal' },
+        { label: 'NATURA DÜNYASI GENÇLERBİRLİĞİ', lig: 'TSL', value: 'ngb' },
+        { label: 'TÜMOSAN KONYASPOR', lig: 'TSL', value: 'tks' },
+        { label: 'HESAP.COM ANTALYASPOR', lig: 'TSL', value: 'hant' },
+        { label: 'İKAS EYÜPSPOR', lig: 'TSL', value: 'ies' },
+        { label: 'KASIMPAŞA A.Ş', lig: 'TSL', value: 'kp' },
+        { label: 'ZECORNER KAYSERİSPOR', lig: 'TSL', value: 'zks' },
+        { label: 'MISIRLI.COM.TR FATİH KARAGÜMRÜK', lig: 'TSL', value: 'mfk' },
+        { label: 'ERZURUMSPOR FK', lig: 'TL', value: 'es' },
+        { label: 'ESENLER EROKSPOR', lig: 'TL', value: 'ees' },
+        { label: 'AMED SPORTİF FAALİYETLER', lig: 'TL', value: 'asf' },
+        { label: 'ARCA ÇORUM FK', lig: 'TL', value: 'açf' },
+        { label: 'ATKO GRUP PENDİKSPOR FUTBOL A.Ş', lig: 'TL', value: 'aps' },
+        { label: 'SİPAY BODRUM FK', lig: 'TL', value: 'sb' },
+        { label: 'BANDIRMA SPOR', lig: 'TL', value: 'bs' },
+        { label: 'EMRE GÖKDEMİR İNŞAAT ANKARA KEÇİÖRENGÜCÜ', lig: 'TL', value: 'akö' },
+        { label: 'ALAGÖZ HOLDİNG IĞDIR FK', lig: 'TL', value: 'aıf' },
+        { label: 'MANİSA FUTBOL KULÜBÜ', lig: 'TL', value: 'mfk' },
+        { label: 'BOLUSPOR', lig: 'TL', value: 'bos' },
+        { label: 'ÖZELSAN SİVASSPOR', lig: 'TL', value: 'öss' },
+        { label: 'EMİNEVİM ÜMRANİYESPOR', lig: 'TL', value: 'eüs' },
+        { label: 'İSTANBULSPOR A.Ş', lig: 'TL', value: 'ist' },
+        { label: 'SMS GRUP SARIYERSPOR', lig: 'TL', value: 'sgs' },
+        { label: 'SAKARYASPOR A.Ş', lig: 'TL', value: 'sas' },
+        { label: 'SERİK SPOR FUTBOL A.Ş', lig: 'TL', value: 'ssf' },
+        { label: 'ATAKAŞ HATAYSPOR', lig: 'TL', value: 'atah' },
+        { label: 'ADANA DEMİRSPOR A.Ş', lig: 'TL', value: 'ads' },
+
+    ]
+
+
+    const [open2, setOpen2] = useState(false);
+    const [value2, setValue2] = useState(null);
+    const [items2, setItems2] = useState([{label: "", value:""}]);
+
+   const Superlig = allTeams.filter((lig) => lig.lig === "TSL")
+   const BırıncıLıg = allTeams.filter((lig) => lig.lig === "TL")
+
+   const Superlıgtakımlar = Superlig.map((team) => team.label)
+   const BırıncıTakımlar = BırıncıLıg.map((team) => team.label)
+
+   console.log(Superlıgtakımlar);
+   console.log(BırıncıTakımlar);
+
+
+   
+
+    // const ligSecildi = (items) => {
+    //     if (items.value == "TSL") {
+
+            
+    //     }
+
+
+    // }
+    // const filtreliTakimlar = allTeams.filter(team => team.lig === lig)
+
+    // setItems2(filtreliTakimlar)
+
+    // setValue2(null)
+
+
     return (
-        <View>
+        <View style={style.container}>
             <HeaderComp name={ScreenName} />
+            <TextInput style={style.input}></TextInput>
+            <TextInput style={style.input} placeholder='İsim'></TextInput>
+            <TextInput style={style.input} placeholder='Soyisim'></TextInput>
+            <View style={style.telefon}>
+                <TextInput style={style.telInput} placeholder='🇹🇷+90'></TextInput>
+                <TextInput style={style.telInput2} placeholder='Telefon Numarası'></TextInput>
+            </View>
+            <TextInput style={style.input} placeholder='E-mail'></TextInput>
+            <TextInput style={style.input} placeholder='Şifre'></TextInput>
+            <DropDownPicker
+                open={open}
+                value={value}
+                items={items}
+                setOpen={setOpen}
+                setValue={setValue}
+                setItems={setItems}
+                zIndex={3000}
+                zIndexInverse={1000}
+                placeholder='Lig Seçiniz'
+            />
+            <DropDownPicker
+                open={open2}
+                value={value2}
+                items={items2}
+                setOpen={setOpen2}
+                setValue={setValue2}
+                setItems={setItems2}
+                zIndex={2000}
+                zIndexInverse={2000}
+                maxHeight={100}
+                placeholder='Takım Seçiniz'
+            />
 
-            <ScrollView>
-                <View style={style.container}>
-                    <TextInput style={style.input} placeholder='İsim'></TextInput>
-                    <TextInput style={style.input} placeholder='Soyisim'></TextInput>
-                    <View style={style.telefon}>
-                        <TextInput style={style.telInput} placeholder='🇹🇷+90'></TextInput>
-                        <TextInput style={style.telInput2} placeholder='Telefon Numarası'></TextInput>
-                    </View>
-                    <TextInput style={style.input} placeholder='E-mail'></TextInput>
-                    <TextInput style={style.input} placeholder='Şifre'></TextInput>
+            <TouchableOpacity style={style.button}>
+                <Text style={style.buttonText}>Kayıt Ol</Text>
+            </TouchableOpacity>
 
-                    <TouchableOpacity style={style.button}>
-                        <Text style={style.buttonText}>Kayıt Ol</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => {
-                        navigation.goBack()
-                    }} style={style.buttonalttext}><Text>Zaten üye misiniz? <Text style={style.title2}>Giriş yapın.</Text></Text></TouchableOpacity>
-                </View>
-            </ScrollView>
-
-
+            <TouchableOpacity onPress={() => {
+                navigation.goBack()
+            }} style={style.buttonalttext}><Text>Zaten üye misiniz? <Text style={style.title2}>Giriş yapın.</Text></Text></TouchableOpacity>
 
         </View>
+
+
 
 
 
@@ -48,6 +151,7 @@ const style = StyleSheet.create({
         flex: 1,
         padding: 25,
         justifyContent: "center",
+        marginTop: -100
     },
     input: {
         backgroundColor: "white",
